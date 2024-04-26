f0fa42ad06c95ce287ea4fc8c23ed99de97a5d95
unknown <makhatadze.mar@gmail.com>
2024-04-26 18:53:50 +0400

    Add variables and loops

---
+++ variables.php
@@ -0,0 +1,192 @@
+<?php
+$slider_content = [
+    [
+        'active' => true,
+        'image' => 'images/slider-img.jpg',
+        'title' => 'Mico',
+        'span' => 'Hospital',
+        'text' => 'when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'button_text' => 'Contact Us',
+        'button_link' => '#'
+    ],
+    [
+        'active' => true,
+        'image' => 'images/slider-img.jpg',
+        'title' => 'Mico',
+        'span' => 'Hospital',
+        'text' => 'when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'button_text' => 'Contact Us',
+        'button_link' => '#'
+    ],
+    [
+        'active' => true,
+        'image' => 'images/slider-img.jpg',
+        'title' => 'Mico',
+        'span' => 'Hospital',
+        'text' => 'when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'button_text' => 'Contact Us',
+        'button_link' => '#'
+    ],
+];
+
+$doctors_info = [
+    [
+        'image' => 'images/team1.jpg',
+        'name' => 'Hennry',
+        'qualification' => 'MBBS',
+        'social_links' => [
+            'facebook' => '#',
+            'twitter' => '#',
+            'linkedin' => '#',
+            'instagram' => '#'
+        ]
+    ],
+    [
+        'image' => 'images/team2.jpg',
+        'name' => 'Jenni',
+        'qualification' => 'MBBS',
+        'social_links' => [
+            'facebook' => '#',
+            'twitter' => '#',
+            'linkedin' => '#',
+            'instagram' => '#'
+        ]
+    ],
+    [
+        'image' => 'images/team3.jpg',
+        'name' => 'Morco',
+        'qualification' => 'MBBS',
+        'social_links' => [
+            'facebook' => '#',
+            'twitter' => '#',
+            'linkedin' => '#',
+            'instagram' => '#'
+        ]
+    ]
+];
+
+$treatments = [
+    [
+        'image' => 'images/t1.png',
+        'title' => 'Nephrologist Care',
+        'description' => 'Alteration in some form, by injected humour, or randomised words which don\'t look even slightly e sure there isn\'t anything.',
+        'link' => '#'
+    ],
+    [
+        'image' => 'images/t2.png',
+        'title' => 'Eye Care',
+        'description' => 'Alteration in some form, by injected humour, or randomised words which don\'t look even slightly e sure there isn\'t anything.',
+        'link' => '#'
+    ],
+    [
+        'image' => 'images/t3.png',
+        'title' => 'Pediatrician Clinic',
+        'description' => 'Alteration in some form, by injected humour, or randomised words which don\'t look even slightly e sure there isn\'t anything.',
+        'link' => '#'
+    ],
+    [
+        'image' => 'images/t4.png',
+        'title' => 'Parental Care',
+        'description' => 'Alteration in some form, by injected humour, or randomised words which don\'t look even slightly e sure there isn\'t anything.',
+        'link' => '#'
+    ]
+];
+
+$testimonials = [
+    [
+        'name' => 'Morijorch',
+        'role' => 'Default model text',
+        'quote' => 'Editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy.',
+    ],
+    [
+        'name' => 'Rochak',
+        'role' => 'Default model text',
+        'quote' => 'Various editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy.',
+    ],
+    [
+        'name' => 'Brad Johns',
+        'role' => 'Default model text',
+        'quote' => 'Various editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy.',
+    ]
+];
+
+$info_contacts = [
+    'location' => [
+        'icon' => 'fa fa-map-marker',
+        'text' => 'Location',
+        'link' => '#'
+    ],
+    'phone' => [
+        'icon' => 'fa fa-phone',
+        'text' => 'Call +01 1234567890',
+        'link' => '#'
+    ],
+    'email' => [
+        'icon' => 'fa fa-envelope',
+        'text' => 'demo@gmail.com',
+        'link' => '#'
+    ]
+];
+
+$social_media_links = [
+    'facebook' => '#',
+    'twitter' => '#',
+    'linkedin' => '#',
+    'instagram' => '#'
+];
+
+$info_links = [
+    'Home' => 'index.html',
+    'About' => 'about.html',
+    'Treatment' => 'treatment.html',
+    'Doctors' => 'doctor.html',
+    'Testimonial' => 'testimonial.html',
+    'Contact us' => 'contact.html'
+];
+
+$latest_posts = [
+    ['image' => 'images/post1.jpg', 'text' => 'Normal distribution'],
+    ['image' => 'images/post2.jpg', 'text' => 'Normal distribution'],
+];
+
+$news_items = [
+    ['image' => 'images/post3.jpg', 'text' => 'Latest news details'],
+    ['image' => 'images/post4.png', 'text' => 'More on the latest news'],
+];
+
+
+$about_image = "images/about-img.jpg";
+$about_text = "has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors";
+
+
+
+$header_info = [
+    'phone' => [
+        'link' => '#', // or your desired phone link
+        'icon' => 'fa fa-phone',
+        'text' => 'Call : +01 123455678990'
+    ],
+    'email' => [
+        'link' => 'mailto:demo@gmail.com', // actual mailto link
+        'icon' => 'fa fa-envelope',
+        'text' => 'Email : demo@gmail.com'
+    ],
+    'location' => [
+        'link' => '#', // or your location link
+        'icon' => 'fa fa-map-marker',
+        'text' => 'Location'
+    ]
+];
+
+$nav_links = [
+    'Home' => 'index.php',
+    'About' => 'about.php',
+    'Treatment' => 'treatment.php',
+    'Doctors' => 'doctor.php',
+    'Testimonial' => 'testimonial.php',
+    'Contact Us' => 'contact.php'
+];
+
+$logo_url = "images/logo.png";
+
+?>
