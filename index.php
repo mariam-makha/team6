3583844394c27e1093d8e2ca1362f91fc8ff6950
Diana Pipia <pipiadiana@gmail.com>
2024-04-26 18:30:35 +0400

    edit testimonial/treatment'

--- index.php
+++ index.php
@@ -348,41 +348,7 @@
 
 
   <!-- treatment section -->
-  <section class="treatment_section layout_padding">
-    <div class="side_img">
-      <img src="images/treatment-side-img.jpg" alt="">
-    </div>
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          Hospital <span>Treatment</span>
-        </h2>
-      </div>
-      <div class="row">
-      <?php
-            $index = 0;
-            $count = count($treatments);
-            while ($index < $count) {
-                $treatment = $treatments[$index];
-                echo '<div class="col-md-6 col-lg-3">';
-                echo '    <div class="box ">';
-                echo '        <div class="img-box">';
-                echo '            <img src="' . ($treatment['img_src']) . '" alt="">';
-                echo '        </div>';
-                echo '        <div class="detail-box">';
-                echo '            <h4>' . ($treatment['title']) . '</h4>';
-                echo '            <p>' . ($treatment['description']) . '</p>';
-                echo '            <a href="' . ($treatment['read_more_link']) . '">Read More</a>';
-                echo '        </div>';
-                echo '    </div>';
-                echo '</div>';
-                $index++;
-            }
-            ?>
-        </div>
-    </div>
-  </section>
-
+  <?php include "./treatment-function.php";?>
   <!-- end treatment section -->
 
   <!-- team section -->
@@ -392,7 +358,17 @@
 
 
   <!-- client section -->
-  <?php include "./treatment-function.php";?>
+  
+  <section class="client_section layout_padding">
+    <div class="container">
+        <div class="heading_container">
+            <h2>
+                <span>Testimonial</span>
+            </h2>
+        </div>
+    </div>
+
+    <?php include "./testimonial-function.php";?>
   <!-- end client section -->
 
   <!-- contact section -->
